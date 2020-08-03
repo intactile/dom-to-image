fc11b0ea1de1a0deb73082a944ae2a044da0bd00
Thomas Paillot <paillot.t@intactile.com>
2020-08-03 14:50:06 +0200

    Transpile code to ES5 with babel before building it with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 
 export default [
   {
     input: 'src/index.js',
-    plugins: [terser()],
+    plugins: [nodeResolve(), babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' })],
     output: {
       file: 'umd/dom-to-image-next.js',
       format: 'umd',
@@ -13,6 +15,35 @@ export default [
   },
   {
     input: 'src/index.js',
+    plugins: [
+      nodeResolve(),
+      babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
+      terser(),
+    ],
+    output: {
+      file: 'umd/dom-to-image-next.min.js',
+      format: 'umd',
+      name: 'domToImage',
+      esModule: false,
+    },
+  },
+  {
+    input: 'src/index.js',
+    external: [/@babel\/runtime/],
+    plugins: [
+      nodeResolve(),
+      babel({
+        babelHelpers: 'runtime',
+        plugins: [
+          [
+            '@babel/plugin-transform-runtime',
+            {
+              useESModules: true,
+            },
+          ],
+        ],
+      }),
+    ],
     output: {
       file: 'esm/index.js',
       format: 'esm',
